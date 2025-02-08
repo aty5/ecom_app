5c7b32be2042b778af2deb51d92aff212959a1b9
aty5 <atybuild@hotmail.com>
2025-02-08 15:54:01 +0100

    springboot backend generated with nx (monorepo)

---
+++ apps/ecom_app_backend/src/main/java/com/aty/ecom/EcomApplication.java
@@ -0,0 +1,13 @@
+package com.aty.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomApplication.class, args);
+	}
+
+}
